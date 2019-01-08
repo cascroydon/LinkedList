01605252fc8f1e246d19089a404c892aede3ef42
Paul Sinnett <paul.sinnett@gmail.com>
2019-01-08 14:36:29 +0000

    custom list class

--- Program.cs
+++ Program.cs
@@ -1,12 +1,151 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 
 class Example
 {
+	public class MyList : IEnumerable
+	{
+		private class Link : IEnumerable
+		{
+			public object content;
+			Link next = null;
+
+			public Link(object content, Link next = null)
+			{
+				this.content = content;
+				this.next = next;
+			}
+
+			public IEnumerator GetEnumerator()
+			{
+				return GetEnumerator(this);
+			}
+
+			static public IEnumerator GetEnumerator(Link link)
+			{
+				return new LinkWalker(link);
+			}
+
+			private class LinkWalker : IEnumerator
+			{
+				Link start;
+				Link current;
+
+				public object Current => current.content;
+
+				public LinkWalker(Link start)
+				{
+					this.start = new Link(null, start);
+					Reset();
+				}
+
+				public bool MoveNext()
+				{
+					current = current.next;
+					return current != null;
+				}
+
+				public void Reset()
+				{
+					current = start;
+				}
+			}
+
+			public Link Next(Link next)
+			{
+				this.next = next;
+				return next;
+			}
+		}
+
+		int count;
+		Link head;
+		Link tail;
+
+		public void Clear()
+		{
+			count = 0;
+			head = null;
+			tail = null;
+		}
+
+		public MyList()
+		{
+			Clear();
+		}
+
+		public IEnumerator GetEnumerator()
+		{
+			return Link.GetEnumerator(head);
+		}
+
+		public bool IsEmpty()
+		{
+			return count == 0;
+		}
+
+		void CreateList(object content)
+		{
+			head = tail = new Link(content);
+			count = 1;
+		}
+
+		public void AddFirst(object content)
+		{
+			if (IsEmpty())
+			{
+				CreateList(content);
+			}
+			else
+			{
+				head = new Link(content, head);
+				count++;
+			}
+		}
+
+		public void AddLast(object content)
+		{
+			if (IsEmpty())
+			{
+				CreateList(content);
+			}
+			else
+			{
+				tail = tail.Next(new Link(content));
+				count++;
+			}
+		}
+
+		public int Count()
+		{
+			return count;
+		}
+
+		public void Concatenate(MyList list)
+		{
+			if (!list.IsEmpty())
+			{
+				if (IsEmpty())
+				{
+					head = list.head;
+					tail = list.tail;
+					count = list.count;
+				}
+				else
+				{
+					tail.Next(list.head);
+					tail = list.tail;
+					count += list.count;
+				}
+				list.Clear();
+			}
+		}
+	}
+
 	static void Main(string[] args)
 	{
-		LinkedList<object> LLs = new LinkedList<object>();
-		LinkedList<object> LLi = new LinkedList<object>();
+		MyList LLs = new MyList();
+		MyList LLi = new MyList();
 
 		LLs.AddLast("Smell");
 		LLs.AddFirst("Dudeo");
@@ -14,9 +153,9 @@ class Example
 		LLi.AddFirst(10);
 		LLi.AddLast(9);
 
-		LLs.AddLast(LLi);
+		LLs.Concatenate(LLi);
 
-		Console.WriteLine("We have the following {0} items in the is the list.", LLs.Count);
+		Console.WriteLine("We have the following {0} items in the is the list.", LLs.Count());
 
 		foreach (var item in LLs)
 		{
--- README.md
+++ README.md
@@ -1,20 +1,5 @@
 # LinkedList
 
-To fix the error we can convert the lists to `LinkedList<object>`.
+To get the behaviour we want exactly we must write or use a different list class. The expected behaviour is that concatenating two lists should be an O(1) operation. That can be achieved with a lot of extra work.
 
-This allows us to compile and run. However now our output is:
-
-    We have the following 3 items in the is the list.
-    Node's data is Dudeo
-    Node's data is Smell
-    Node's data is System.Collections.Generic.LinkedList`1[System.Object]
-
-The line:
-
-    LLs.AddLast(LLi);
-
-It isn't joining lists `LLi` and `LLs` together, it's adding list `LLi` as an element to the end of list `LLs`. `LinkedList<object>` is also a type of `object` and is added like any other element.
-
-Is there an alternative function that we call to join these lists instead? [Unfortunately not](https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.linkedlist-1?view=netframework-4.7.2]):
-
-> The `LinkedList<T>` class does not support chaining, splitting, cycles, or other features that can leave the list in an inconsistent state. The list remains consistent on a single thread. The only multithreaded scenario supported by `LinkedList<T>` is multithreaded read operations.
+In this version I have made the `Concatenate` function destructive, that is, it moves the contents of one list into the other. The input list `LLi` will be empty after the operation. This is desirable to prevent the two list instances corrupting the other's content. For example, if the list `LLi` was not cleared by the `Concatenate` function, additional elements could be added to it afterwards which would then invalidate the `tail` and `count` parameters of `LLs`.
